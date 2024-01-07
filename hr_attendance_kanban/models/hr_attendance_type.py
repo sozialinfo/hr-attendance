e8374bb4315d96069205ec84d12ecf688c1327a3
Miika Nissi <miika@miikanissi.com>
2024-01-07 01:45:50 -0500

    [IMP] hr_attendance_kanban: Check In / Out Wizard
    Initial build for hr_attendance_kanban check in / out wizard by dragging
    employee on attendance kanban.

--- hr_attendance_kanban/models/hr_attendance_type.py
+++ hr_attendance_kanban/models/hr_attendance_type.py
@@ -1,8 +1,7 @@
-# Copyright 2023 Odoo S.A.
-# Copyright 2023 ForgeFlow, S.L.
+# Copyright 2023 Verein sozialinfo.ch
 # License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
 
-from odoo import fields, models, api
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
@@ -19,48 +18,42 @@ class HrAttendanceType(models.Model):
         index=True,
     )
     absent = fields.Boolean(
-        string="Absent",
-        help="Defines that the Attendance Type represents the employee being absent. Only one Attendance Type can represent 'absent' and exactly one Attendance Type needs to be defined as 'absent'",
-        copy=False
+        help=(
+            "Defines that the Attendance Type represents the employee being absent."
+            " Only one Attendance Type can represent 'absent' and exactly one"
+            " Attendance Type needs to be defined as 'absent'"
+        ),
+        copy=False,
     )
+    fold = fields.Boolean(string="Folded in Kanban")
 
-    @api.onchange('absent')
-    def _check_only_one_absent(self):
-
-        # Get all attendance types which are defined as 'absent'
-        absent_attendance_types = self.env['hr.attendance.type'].search(
-                [('absent', '=', True)],
-                order = 'sequence'
+    @api.constrains("absent")
+    def _constrains_only_one_absent(self):
+        for attendance_type in self:
+            other_absent_attendance_types = self.env["hr.attendance.type"].search(
+                [("id", "!=", attendance_type.id), ("absent", "=", True)]
             )
-
-        if len(absent_attendance_types) == 1:
-
-            if absent_attendance_types.id == self.id.origin:
-                # This was the only attendance type defined as 'absent' and the user
-                # wants to uncheck it. That's a no-go.
-                raise UserError("You cannot uncheck this Attendance Type as 'absent'. There needs to be exactly one Attendance Type with 'absent' checked, because it is used to represents absent employees. You can however mark another Attendance Type as 'absent' and then the flag for this record will automatically be removed.")
-            else:
-                # There was already _another_ attendance type defined as 'absent'.
-                # Uncheck that one: there may only ever be one.
-                absent_attendance_types.write({'absent': False})
-
-        if len(absent_attendance_types) > 1:
-            # This should never happen... but just in case.
-            # If there is more than one attendance type as 'absent', just mark the first one.
-            absent_attendance_types.write({'absent': False})
-            absent_attendance_types[0].write({'absent': True})
+            if attendance_type.absent and other_absent_attendance_types:
+                other_absent_attendance_types.absent = False
+            elif not attendance_type.absent and not other_absent_attendance_types:
+                raise UserError(
+                    _(
+                        "You cannot uncheck this Attendance Type as 'absent'. There"
+                        " needs to be exactly one Attendance Type with 'absent'"
+                        " checked, because it is used to represents absent"
+                        " employees. You can however mark another Attendance Type"
+                        " as 'absent' and then the flag for this record will"
+                        " automatically be removed."
+                    )
+                )
 
     @api.ondelete(at_uninstall=False)
     def _unlink_except_absent(self):
         if any(attendance_type.absent for attendance_type in self):
-            raise UserError("You cannot delete the Attendance Type marked as 'absent'. There needs to be exactly one Attendance Type with 'absent' checked, because it is used to represents absent employees.")
-
-    @api.ondelete(at_uninstall=False)
-    def _unlink_except_used(self):
-        for attendance_type in self:
-            used_attendance_types = self.env['hr.attendance'].search_count(
-                    [('attendance_type_id', '=', attendance_type.id)]
+            raise UserError(
+                _(
+                    "You cannot delete the Attendance Type marked as 'absent'. There"
+                    " needs to be exactly one Attendance Type with 'absent' checked,"
+                    " because it is used to represents absent employees."
                 )
-            if used_attendance_types > 0:
-                raise UserError("You cannot delete this Attendance Type because it was already used.")
-        
+            )
